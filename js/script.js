3f92b3e4af7a731971eb09e2d37d8ddc6ff6f77c
Artyom <Psixart1@gmail.com>
2020-04-25 12:51:56 +0300

    add map from GoogleMapsAPI

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+const background = document.querySelector('.container-fourteen__background');
+
+
 function initMap() {
   var pos = {lat: -15.307911, lng: 124.261812};
   var opt = {
     center: pos,
     zoom: 17,
-    disableDefaultUI: true,
     scrollwheel: false,
     mapTypeId: 'satellite'
   }
@@ -12,7 +14,10 @@ function initMap() {
     position: pos,
     map: myMap,
     icon: '../img/maps-and-flags.svg',
-    animation: google.maps.Animation.DROP,
-    
+    animation: google.maps.Animation.BOUNCE
   })
 }
+
+background.onclick = function() {
+  background.style.display = 'none';
+  };
