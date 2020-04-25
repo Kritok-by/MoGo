f19588298bed0d8287700f0f490b8cb7dadbaee6
Artyom <Psixart1@gmail.com>
2020-04-25 13:13:49 +0300

    small fixes

--- js/script.js
+++ js/script.js
@@ -2,20 +2,20 @@ const background = document.querySelector('.container-fourteen__background');
 
 
 function initMap() {
-  var pos = {lat: -15.307911, lng: 124.261812};
-  var opt = {
-    center: pos,
-    zoom: 17,
-    scrollwheel: false,
-    mapTypeId: 'satellite'
-  }
-  var myMap = new google.maps.Map(document.getElementById('map'), opt);
-  var marker = new google.maps.Marker({
-    position: pos,
-    map: myMap,
-    icon: '../img/maps-and-flags.svg',
-    animation: google.maps.Animation.BOUNCE
-  })
+  var pos = {lat: -15.307911, lng: 124.261812},
+      opt = {
+        center: pos,
+        zoom: 17,
+        mapTypeId: 'satellite'
+      },
+      myMap = new google.maps.Map(document.getElementById('map'), opt),
+      markerImage = '../img/maps-and-flags.svg',
+      marker = new google.maps.Marker({
+        position: pos,
+        map: myMap,
+        // icon: markerImage,
+        animation: google.maps.Animation.BOUNCE
+      })
 }
 
 background.onclick = function() {
