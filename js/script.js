b7bdc8f7b9cf8b6a19812deec19f48517bc20403
Artyom <Psixart1@gmail.com>
2020-05-04 04:00:44 +0300

    add all blocks

--- js/script.js
+++ js/script.js
@@ -1,5 +1,54 @@
 const background = document.querySelector('.container-fourteen__background');
 
+document.getElementById('burger').onclick = function(){
+  addMenu();
+};
+
+function addMenu(){
+  document.getElementById('menu').classList.toggle('show');
+};
+
+setInterval(function(){
+  if(document.querySelectorAll('.owl-dot')[0].className === 'owl-dot active'){
+      document.querySelector('.container-one').classList.remove('container1_img2');
+      document.querySelector('.container-one').classList.remove('container1_img3');
+      document.querySelector('.container-one').classList.remove('container1_img4');
+      document.querySelector('.container-one').classList.add('container1_img1');
+  } else if(document.querySelectorAll('.owl-dot')[1].className === 'owl-dot active'){
+    document.querySelector('.container-one').classList.remove('container1_img1');
+    document.querySelector('.container-one').classList.remove('container1_img3');
+    document.querySelector('.container-one').classList.remove('container1_img4');
+    document.querySelector('.container-one').classList.add('container1_img2');
+  } else if(document.querySelectorAll('.owl-dot')[2].className === 'owl-dot active'){
+    document.querySelector('.container-one').classList.remove('container1_img1');
+    document.querySelector('.container-one').classList.remove('container1_img2');
+    document.querySelector('.container-one').classList.remove('container1_img4');
+    document.querySelector('.container-one').classList.add('container1_img3');
+  } else if(document.querySelectorAll('.owl-dot')[3].className === 'owl-dot active'){
+    document.querySelector('.container-one').classList.remove('container1_img1');
+    document.querySelector('.container-one').classList.remove('container1_img3');
+    document.querySelector('.container-one').classList.remove('container1_img2');
+    document.querySelector('.container-one').classList.add('container1_img4');
+    
+  }
+}, 10);
+setInterval(function(){
+  if(document.querySelectorAll('.owl-dot').className === 'owl-dot active'){
+    document.querySelector('.owl-dot ative').classList.add('dotGrad');
+  }
+}, 10);
+
+
+
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        dots: true,
+        loop: true
+    });
+  });
+
+
  function initMap() {
   var pos = {lat: -15.307911, lng: 124.261812},
       opt = {
@@ -25,5 +74,8 @@ background.onclick = function() {
   background.style.display = 'none';
   };
 
+  
 
   new WOW().init();
+
+  
